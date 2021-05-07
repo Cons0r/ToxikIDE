f0b19a8194f50997f3e96872a1f796da4c792348
maverickquill55 <redrangercj@icloud.com>
2021-05-07 16:22:20 +0000

    added feature to turn on virus mode without having to download it seperatly

--- inject.py
+++ inject.py
@@ -1,6 +1,8 @@
 import replit.audio
 import platform
 import os
+import tkinter
+
 def run():
 	replit.audio.play_file('sound.mp3', 1, False, 0, 'jalert')
 def allowedOS():
@@ -10,3 +12,6 @@ def allowedOS():
 		return False
 def getOS():
 	return platform.system()
+def inject():
+	os.rename('main.py','main2.py')
+	os.rename('inject.py','main.py')
--- virus.py
+++ virus.py
@@ -13,9 +13,48 @@ file_path = ''
 def set_file_path(path):
     global file_path
     file_path = path
+
+
 def AllowVirus():
-	inject.inject()
-	
+    inject.run()
+
+
+for i in range(100):
+    i2 = i + 1
+    allowed = inject.allowedOS()
+    OS = inject.getOS()
+    if i2 <= 30 and allowed:
+        tkinter.messagebox.showwarning(
+            title="bye bye computer no. {}".format(i2),
+            message=
+            "that was a bad idea :/ dont try restarting, it makes it worse")
+        inject.run()
+    elif i2 <= 50 and allowed:
+        tkinter.messagebox.showwarning(
+            title="bye bye computer",
+            message="well i guess dont press ads like that")
+        inject.run()
+    elif i2 <= 70 and allowed:
+        tkinter.messagebox.showwarning(title="bye bye computer",
+                                       message="it was just a prank bro")
+        inject.run()
+    elif i2 == 99 and allowed:
+        tkinter.messagebox.showwarning(
+            title="bye bye computer",
+            message="why do you need robux/vbucks/minecoins anyway?")
+        inject.run()
+    elif i2 == 100 and allowed:
+        tkinter.messagebox.showwarning(title="bye bye computer",
+                                       message="bye for now..")
+        inject.run()
+    elif not allowed:
+        tkinter.messagebox.showwarning(
+            title="FATAL ERR",
+            message=
+            "PYTHON PAGACKE GAME.CURRENCY.get: COMPUTER.TYPE {} NOT SUPPORTED".
+            format(OS))
+
+
 def open_file():
     path = askopenfilename(filetypes=[('Python Files', '*.py')])
     with open(path, 'r') as file:
@@ -43,10 +82,13 @@ def run():
         text.pack()
         return
     command = f'python {file_path}'
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    process = subprocess.Popen(command,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE,
+                               shell=True)
     output, error = process.communicate()
     code_output.insert('1.0', output)
-    code_output.insert('1.0',  error)
+    code_output.insert('1.0', error)
 
 
 menu_bar = Menu(compiler)
@@ -58,7 +100,6 @@ file_menu.add_command(label='Save', command=save_as)
 file_menu.add_command(label='Save As', command=save_as)
 file_menu.add_command(label='Exit', command=exit)
 menu_bar.add_cascade(label='File', menu=file_menu)
-
 
 run_bar = Menu(menu_bar, tearoff=0)
 run_bar.add_command(label='Run', command=run)
